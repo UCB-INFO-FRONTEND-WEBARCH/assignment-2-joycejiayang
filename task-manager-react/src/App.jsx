7e4cc765c3508a07b84c75ab8e048c2c642c9b7f
Joyce Yang <yangjoyce02@gmail.com>
2025-11-18 13:52:50 -0800

    added header to project, added a few base styles

--- task-manager-react/src/App.jsx
+++ task-manager-react/src/App.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
+import menuIcon from '../src/assets/menu_icon.png'
+import searchIcon from '../src/assets/search_icon.png'
+import checkIcon from '../src/assets/check_icon.png'
 
 function App() {
   return (
     <>
       <header className="header">
-        <input type="checkbox" id="menu-toggle" name="menu-toggle" checked>
-        {/* <label id="menu-label" for="menu-toggle">
-          <img className="menu icon" alt="Menu Icon" src="assets/menu_icon.png">
-        </label> */}
+        <input type="checkbox" id="menu-toggle" name="menu-toggle" checked />
+        <label id="menu-label" for="menu-toggle">
+          <img className="menu icon" alt="Menu Icon" src={menuIcon} />
+        </label>
           
-        {/* <div className="search-bar">
-            <img class="icon" alt="Search Icon" src="assets/search_icon.png">
-            <input type="text" placeholder="Quick Find">
+        <div className="search-bar">
+            <img className="icon" alt="Search Icon" src={searchIcon} />
+            <input type="text" placeholder="Quick Find" />
         </div>
 
         <div className="task-counter">
-            <img className="icon" alt="Task Counter Icon" src="assets/check_icon.png">
+            <img className="icon" alt="Task Counter Icon" src={checkIcon} />
             <span>30/5</span>
-        </div> */}
+        </div>
       </header>
     </>
   )
