9df44eb7b03df66058939a7395d5b1cf05632b90
Joyce Yang <yangjoyce02@gmail.com>
2025-11-18 20:22:42 -0800

    implemented filter functionality

--- task-manager-react/src/components/TaskList.jsx
+++ task-manager-react/src/components/TaskList.jsx
@@ -4,6 +4,7 @@ import './TaskList.css'
 
 function TaskList({taskArray, inboxType}) {
     const [tasks, setTasks] = useState(taskArray)
+    const [filter, setFilter] = useState("All")
 
     function handleToggle(id) {
         setTasks(prevTasks =>
@@ -26,32 +27,42 @@ function TaskList({taskArray, inboxType}) {
 
             <ul className="task-filters">
                 <li>
-                    <input type="radio" id="task-filter-all" name="filters" defaultChecked />
+                    <input type="radio" id="task-filter-all" name="filters" onChange={() => setFilter("All")} defaultChecked />
                     <label className="task-filter-item" htmlFor="task-filter-all">
                         All
                     </label>
                 </li>
                 <li>
-                    <input type="radio" id="task-filter-active" name="filters" defaultChecked />
+                    <input type="radio" id="task-filter-active" name="filters" onChange={() => setFilter("Active")} />
                     <label className="task-filter-item" htmlFor="task-filter-active">
                         Active
                     </label>
                 </li>
                 <li>
-                    <input type="radio" id="task-filter-completed" name="filters" defaultChecked />
+                    <input type="radio" id="task-filter-completed" name="filters" onChange={() => setFilter("Completed")} />
                     <label className="task-filter-item" htmlFor="task-filter-completed">
                         Completed
                     </label>
                 </li>
             </ul>
 
-            {/* Use unordered lists for navigation and task items */}
+            {/* Step 1: Filter by inbox type 
+            Step 2: Filter by filter type 
+            Step 3: Map onto TaskItems */}
             <ul className="task-list">
                 {tasks.filter(taskObject => {
                     if (inboxType === "Inbox") {
                         return true;
                     }
                     return taskObject.inboxType === inboxType;
+                }).filter(taskObject => {
+                    if (filter === "Active") {
+                        return !taskObject.completed;
+                    } else if (filter === "Completed") {
+                        return taskObject.completed
+                    } else {
+                        return true;
+                    }
                 }).map(taskObject => 
                     <TaskItem 
                         key={taskObject.id} 
